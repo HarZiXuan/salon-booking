6f1bd658b72c9ed4b3d92daad1b0234f9083191f
Har Zi Xuan <harzixuan@gmail.com>
2026-03-07 16:31:29 +0800

    remove 2024 reserved

--- app/(store)/layout.tsx
+++ app/(store)/layout.tsx
@@ -101,13 +101,6 @@ export default function StoreLayout({
                 </div>
             </header>
             <main className="flex-1 select-none">{children}</main>
-            <footer className="border-t py-6 md:py-0">
-                <div className="container flex flex-col items-center justify-between gap-4 md:h-24 md:flex-row">
-                    <p className="text-center text-sm leading-loose text-muted-foreground md:text-left">
-                        © 2024 SalonBook. All rights reserved.
-                    </p>
-                </div>
-            </footer>
         </div>
     );
 }
